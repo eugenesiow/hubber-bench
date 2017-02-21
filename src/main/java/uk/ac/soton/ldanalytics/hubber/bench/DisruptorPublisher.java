d5fd8c61e3f1c33f56672a11433e27222bdc5518
Eugene Siow <kyo116@gmail.com>
2017-02-21 14:56:02 +0000

    Add single connection

--- src/main/java/uk/ac/soton/ldanalytics/hubber/bench/DisruptorPublisher.java
+++ src/main/java/uk/ac/soton/ldanalytics/hubber/bench/DisruptorPublisher.java
@@ -1,21 +1,53 @@
 package uk.ac.soton.ldanalytics.hubber.bench;
 
+import java.util.UUID;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
 
 public class DisruptorPublisher {
+	static String topic        = "MQTT Examples";
+    static int qos             = 1;
+    static String broker       = "tcp://iot.eclipse.org:1883";
+    static MemoryPersistence persistence = new MemoryPersistence();
+    static MqttClient sampleClient;
+    
+	
 	public static void handleEvent(Point event, long sequence, boolean endOfBatch) {
-		System.out.println(event.getMsg());
+		try {
+			System.out.println(event.getMsg());
+			MqttMessage message = new MqttMessage(event.getMsg().getBytes());
+			message.setQos(qos);
+			sampleClient.publish(topic, message);
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
     }
 
-    public static void translate(Point event, long sequence, String msg) {
-    	event.set(msg);
+    public static void translate(Point event, long sequence, String[] msg) {
+    	event.set(Long.parseLong(msg[0]),msg[1]);
     }
 	
 	public static void main(String[] args) throws Exception {
+		String clientId     = UUID.randomUUID().toString();
+		try {
+			sampleClient = new MqttClient(broker, clientId, persistence);
+			MqttConnectOptions connOpts = new MqttConnectOptions();
+//	        connOpts.setCleanSession(true);
+	        sampleClient.connect(connOpts);
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
+		
+		
         // Executor that will be used to construct new threads for consumers
         Executor executor = Executors.newCachedThreadPool();
 
@@ -36,7 +68,7 @@ public class DisruptorPublisher {
         
 
         while (!Thread.currentThread ().isInterrupted ()) {
-        	String msg = "test";
+        	String[] msg = {"0","test"};
         	ringBuffer.publishEvent(DisruptorPublisher::translate, msg);
         }
     }
--- src/main/java/uk/ac/soton/ldanalytics/hubber/bench/Point.java
+++ src/main/java/uk/ac/soton/ldanalytics/hubber/bench/Point.java
@@ -2,9 +2,15 @@ package uk.ac.soton.ldanalytics.hubber.bench;
 
 public class Point {
 	private String msg;
-
-    public void set(String msg) {
+	private Long publisher;
+    
+    public void set(Long publisher, String msg) {
+    	this.publisher = publisher;
         this.msg = msg;
+    }
+    
+    public long getPublisher() {
+    	return publisher;
     }
     
     public String getMsg() {
